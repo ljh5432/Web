19336023ecca5a64f939c6068a7f14cfa44f4f2b
지현 이 <ljh5432@gmail.com>
2019-01-15 14:16:55 +0900

    데이터베이스 테스트

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Test;
+using MySql.Data.MySqlClient;
 
 namespace Web.Controllers
 {
@@ -15,8 +16,15 @@ namespace Web.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            Class1 c1 = new Class1();
-            Console.WriteLine(c1.GetInt());
+            DataBase db = new DataBase();
+            // dotnet add package MySql.Data --version 8.0.13 //설치 필요
+            MySqlConnection conn = db.GetConnection();
+            if(conn == null){
+                Console.WriteLine("접속 오류!");
+            }
+            else {
+                Console.WriteLine("접속 성공!");
+            }
             return new string[] { "value1", "value2" };
         }
 
