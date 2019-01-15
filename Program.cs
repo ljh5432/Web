4649c15071785000bb0c6e9cc33007536d1c678f
ljh5432 <best_ljh@naver.com>
2019-01-15 10:19:29 +0900

    80포트 변경

--- Program.cs
+++ Program.cs
@@ -19,7 +19,7 @@ namespace Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>()
                 .UseUrls("http://*:80");
     }
 }
