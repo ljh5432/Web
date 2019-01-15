62de767de601ec898e483a5d61f7dc5b0094b056
ljh5432 <best_ljh@naver.com>
2019-01-15 10:14:28 +0900

    80포트추가

--- Program.cs
+++ Program.cs
@@ -20,5 +20,6 @@ namespace Web
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
+                .UseUrls("http://*:80");
     }
 }
